1a3f329831f95335a59274a5e4ce3c100102b1b0
Kratik Jain <kratik1998@gmail.com>
2019-06-13 16:22:13 +0530

    add files via upload

---
+++ Binary Search Tree.py
@@ -0,0 +1,50 @@
+#()
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.leftChild = None
+        self.rightChild = None
+
+class BinartSearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self,data):
+        if not self.root:
+            self.root = Node(data)
+        else:
+            self.insertNode(data, self.root)
+
+
+    def insertNode(self, data, root_node):
+        if data < root_node.data:
+            if root_node.leftChild:
+                self.insertNode(data,root_node.leftChild)
+            else:
+                root_node.leftChild = Node(data)
+
+        else:
+            if root_node.rightChild:
+                self.insertNode(data,root_node.rightChild)
+            else:
+                root_node.rightChild = Node(data)
+
+    def traverse(self):
+        if self.root:
+            self.traverseInOrder(self.root)
+
+    def traverseInOrder(self, root_node):
+
+        if root_node.leftChild:
+            self.traverseInOrder(root_node.leftChild)
+
+        print("%s kj" % root_node.data)
+
+        if root_node.rightChild:
+            self.traverseInOrder(root_node.rightChild)
+
+v = BinartSearchTree()
+v.insert(10)
+v.insert(5)
+v.insert(15)
+v.insert(6)
